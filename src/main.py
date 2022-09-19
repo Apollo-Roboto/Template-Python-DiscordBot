f61ab52febab166bc2a8c0fccf9c00aa407b47fd
Apollo <tiralex@hotmail.com>
2022-09-19 03:15:37 -0400

    added Sync controller to update slash controller + status controller + main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,33 @@
-from dotenv import load_dotenv
+import asyncio
+from discord.ext import commands
+from discord import Intents
 
-def main():
+from dotenv import load_dotenv
+import os
+
+from controllers.StatusController import StatusController
+from controllers.SyncController import SyncController
+
+async def main():
 	load_dotenv()
 
+	bot = commands.Bot(
+		command_prefix='!',
+		intents=Intents.all(),
+	)
+
+	await bot.add_cog(SyncController(bot)) 
+	await bot.add_cog(StatusController(bot)) 
+
+	print("App Commands:")
+	for command in bot.tree.walk_commands():
+		print(f'\t{command.name}')
+	print("Commands:")
+	for command in bot.walk_commands():
+		print(f'\t{command.name}')
+
+	# bot.run(os.environ['DISCORD_BOT_TOKEN'])
+	await bot.start(os.environ['DISCORD_BOT_TOKEN'])
+
 if(__name__ == '__main__'):
-	main()
+	asyncio.run(main())
---
+++ src/views/StatusView.py
@@ -0,0 +1,12 @@
+from discord import Embed, Color
+from models.Status import Status
+
+color = Color.from_str('#5865f2')
+
+def view(status: Status):
+	embed = Embed(color=color)
+	embed.title = "Current Status"
+	embed.add_field(name='Ready', value=status.ready, inline=True)
+	embed.add_field(name='CPU', value=f'{status.cpu}%', inline=True)
+	embed.add_field(name='RAM', value=f'{status.ram}%', inline=True)
+	return embed
